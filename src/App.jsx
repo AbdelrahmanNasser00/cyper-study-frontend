04fd8eeeea72a727ad9aa9cfc0e6df168e5562b6
Abdelrhman143 <aa632481@gmail.com>
2025-05-05 21:01:52 +0300

    fixing problem of import wishlist and coursedetails

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,8 @@ import Register from "./pages/auth/register/Register";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound";
 import GuestRoute from "./components/GuestRoute";
-<<<<<<< HEAD
 import WishList from "./components/WishList";
-=======
 import CourseDetails from "./pages/CourseDetails";
->>>>>>> 54de6c3e554697aa1918fbb3dd4565355368e41c
 
 function App() {
   return (
@@ -18,12 +15,11 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/wishlist" element={<WishList/>} />
+        <Route path="/wishlist" element={<WishList />} />
       </Route>
       <Route element={<GuestRoute />}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
       </Route>
       <Route path="/details" element={<CourseDetails></CourseDetails>}></Route>
     </Routes>
