78045b065db65d75046d65cd7f7bee56028b2e1e
AbdelrahmanNasser00 <abdo2652149@gmail.com>
2025-05-27 09:41:05 +0300

    Add certificate Api

---
+++ src/services/certificateApi.js
@@ -0,0 +1,55 @@
+import { baseApi } from "../baseApi";
+
+export const certificatesApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    generateCertificate: builder.mutation({
+      query: (data) => ({
+        url: "/certificates",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Certificates"],
+    }),
+
+    getAllCertificates: builder.query({
+      query: () => ({
+        url: "/certificates",
+        method: "GET",
+      }),
+      providesTags: ["Certificates"],
+    }),
+
+    getMyCertificates: builder.query({
+      query: () => ({
+        url: "/certificates/my-certificates",
+        method: "GET",
+      }),
+      providesTags: ["Certificates"],
+    }),
+
+    getCertificateById: builder.query({
+      query: (id) => ({
+        url: `/certificates/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["Certificates"],
+    }),
+
+    deleteCertificate: builder.mutation({
+      query: (id) => ({
+        url: `/certificates/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Certificates"],
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const {
+  useGenerateCertificateMutation,
+  useGetAllCertificatesQuery,
+  useGetMyCertificatesQuery,
+  useGetCertificateByIdQuery,
+  useDeleteCertificateMutation,
+} = certificatesApi;
