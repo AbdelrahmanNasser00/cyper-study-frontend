17bdc074c23789244e29018d9ed36fddc0024aa4
Abdelrhman143 <aa632481@gmail.com>
2025-05-27 16:37:50 +0300

    binding certificate with api

--- src/pages/StudentDashboard/components/CertificateTab.jsx
+++ src/pages/StudentDashboard/components/CertificateTab.jsx
@@ -1,6 +1,11 @@
 import CertificateCard from "./CertificateCard";
+import { useGetMyCertificatesQuery } from "@/services/certificateApi";
 
 function CertificateTab() {
+  const { data: certificates = [], isLoading } = useGetMyCertificatesQuery();
+
+  console.log(certificates);
+
   return (
     <div className="border-2 border-gray-100 rounded-lg p-5">
       <div className="mb-5">
@@ -10,11 +15,11 @@ function CertificateTab() {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 gap-5">
-        <CertificateCard></CertificateCard>
-        <CertificateCard></CertificateCard>
-        <CertificateCard></CertificateCard>
-        <CertificateCard></CertificateCard>
-        <CertificateCard></CertificateCard>
+        {isLoading
+          ? "Loading..."
+          : certificates.map((certificate) => (
+              <CertificateCard key={certificate.id} certificate={certificate} />
+            ))}
       </div>
     </div>
   );
